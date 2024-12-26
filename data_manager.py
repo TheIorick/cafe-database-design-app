a901df55c228319724f449984fc95468f9505219
The_Iorick <kotolevskij.vlad@mail.ru>
2024-12-26 14:59:38 +0300

    final commit

--- data_manager.py
+++ data_manager.py
@@ -8,16 +8,6 @@ fake = Faker('ru_RU')
 
 
 class DataManager:
-    def populate_suppliers(self, n):
-        for _ in range(n):
-            supplier = Supplier(
-                delivery_price=random.randint(1000, 10000),
-                company_name=fake.company()
-            )
-            db.session.add(supplier)
-        db.session.commit()
-        print(f"{n} suppliers added to the database.")
-
     def populate_table(self, table_name, n):
         """
         Заполняет указанную таблицу тестовыми данными.
